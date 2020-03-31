cc18730abbee86dafa5b0ebcc5c031f77f0f29ff
Gyanesh Mahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2020-03-31 08:03:18 +0530

    Update 2D_array.c

--- Array/2D_array.c
+++ Array/2D_array.c
@@ -4,7 +4,7 @@
 int main()
 {
     //1st Method: creating 2D array in stack
-    int A[3][4]={{1,2,3,4}, {2,4,6,8}, {3,5,7,9}}; //It will be created inside stack section.
+    int A[3][4]={{1,2,3,4}, {2,4,6,8}, {3,5,7,9}}; //This array will be created inside stack section.
     printf("printing elements of array A:\n");
     for(int i=0; i<3; i++)
     {
@@ -16,10 +16,10 @@ int main()
     printf("\n");
 
     //2nd Method: Array of pointers
-    int *B[3];  //It will be created in stack
-    B[0]=(int*)malloc(sizeof(int)*4);   //This array will be created in heap.
-    B[1]=(int*)malloc(sizeof(int)*4);   //This array will be created in heap.
-    B[2]=(int*)malloc(sizeof(int)*4);   //This array will be created in heap.
+    int *B[3];  //It will be created inside stack section of memory.
+    B[0]=(int*)malloc(sizeof(int)*4);   //This array will be created inside heap section of memory.This array will be created in heap.
+    B[1]=(int*)malloc(sizeof(int)*4);   //This array will be created inside heap section of memory.
+    B[2]=(int*)malloc(sizeof(int)*4);   //This array will be created inside heap section of memory.
 
     int k=1;
     printf("Initializing array B with square of 2");
