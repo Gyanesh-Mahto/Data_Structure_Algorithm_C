b82b3942db32cec037a362bd60a9745dc3233ab7
GMahto <61919561+Gyanesh-Mahto@users.noreply.github.com>
2022-10-14 12:18:33 +0530

    Sum of circular array values for given count+Index

---
+++ Array/sum_of_circular_array.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int count = 0, index = 0, sum = 0, temp = 0;
+    bool flag = false;
+    cout<<"Count: ";
+    cin>>count;
+    temp = count;
+    cout<<"Index: ";
+    cin>>index;
+
+    for(index--; temp>0;)
+    {
+        if(index >= temp)
+        {
+            sum+=arr[index];
+            index--;
+            temp--;
+        } 
+        
+        if((index < temp) && (index !=0))
+        {
+            sum+=arr[index];
+            index--;
+            temp--;
+        }
+        
+        if((index == 0) && (flag == false))
+        {
+            sum+=arr[index];
+            flag = true;
+            index = 9;
+            temp--;
+        }
+        
+    }
+    
+    cout << "Sum: "<< sum << endl;
+    
+    return 0;
+}
